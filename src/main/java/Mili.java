80ce5b043f9dbd10fb033c543e83971037b976fc
Xia Tangzihan <miller.xia2015@gmail.com>
2026-01-20 10:04:59 +0800

    Rename 'Mili', added skeleton greeting and exit.

---
+++ src/main/java/Mili.java
@@ -0,0 +1,20 @@
+public class Mili {
+    public static void main(String[] args) {
+        String logo = " __  __  _  _  _ \n"
+                + "|  \\/  |(_)| |(_)\n"
+                + "| |\\/| || || | _ \n"
+                + "| |  | || || || |\n"
+                + "|_|  |_||_||_||_|\n";
+        System.out.println("Hello from\n" + logo);
+
+        String hor_div_line = "------------------------------------------------";
+        String greeting = "Hello! I'm Mili \nWhat can I do for you?\n";
+        String byebye = "Bye. Hope to see you again soon!";
+
+        System.out.println(hor_div_line);
+        System.out.println(greeting);
+        System.out.println(hor_div_line);
+        System.out.println(byebye);
+        System.out.println(hor_div_line);
+    }
+}
