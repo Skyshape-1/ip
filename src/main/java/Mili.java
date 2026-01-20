47fa7bb2a5ea2bc46c5370d1f5c7392443df5638
Xia Tangzihan <miller.xia2015@gmail.com>
2026-01-20 10:26:02 +0800

    feat(echo): conforming to pattern in greet and exit.

--- src/main/java/Mili.java
+++ src/main/java/Mili.java
@@ -1,14 +1,23 @@
+import java.util.Scanner;
+
 public class Mili {
     public static void main(String[] args) {
-        // Your specific variable name
         String HOR_DIV_LINE = "------------------------------------------------";
 
         printLogo();
         greet(HOR_DIV_LINE);
 
-        // This exit call is for Level-0.
-        // In Level-1, you will wrap this in a loop.
-        exit(HOR_DIV_LINE);
+        Scanner sc = new Scanner(System.in);
+
+        String nextMessage = sc.nextLine();
+        while(true) {
+            if (nextMessage.equals("bye")) {
+                exit(HOR_DIV_LINE);
+                break;
+            }
+            echo(nextMessage, HOR_DIV_LINE);
+            nextMessage = sc.nextLine();
+        }
     }
 
     private static void printLogo() {
@@ -20,19 +29,26 @@ public class Mili {
         System.out.println("Hello from\n" + logo);
     }
 
-    public static void greet(String hor_div_line) {
-        // Your specific variable name
+    private static void greet(String hor_div_line) {
         String greeting = "Hello! I'm Mili \nWhat can I do for you?\n";
         System.out.println(hor_div_line);
         System.out.println(greeting);
         System.out.println(hor_div_line);
+        System.out.println("\n");
     }
 
-    public static void exit(String hor_div_line) {
-        // Your specific variable name
+    private static void echo(String userMessage, String hor_div_line) {
+        System.out.println(hor_div_line);
+        System.out.println(userMessage);
+        System.out.println(hor_div_line);
+        System.out.println("\n");
+    }
+
+    private static void exit(String hor_div_line) {
         String byebye = "Bye. Hope to see you again soon!";
         System.out.println(hor_div_line);
         System.out.println(byebye);
         System.out.println(hor_div_line);
+        System.out.println("\n");
     }
 }
